e84a81a17a6f7ff2901d426240d09d9db9b0ebe1
James Tooze <toozej@gmail.com>
2023-01-07 12:01:27 -0800

    init dinnerclub from golang-starter template

--- scripts/manpages.sh
+++ scripts/manpages.sh
@@ -2,4 +2,4 @@
 set -e
 rm -rf manpages
 mkdir manpages
-go run ./cmd/golang-starter/ man | gzip -c -9 >manpages/golang-starter.1.gz
+go run ./cmd/dinnerclub/ man | gzip -c -9 >manpages/dinnerclub.1.gz
--- scripts/use_starter.sh
+++ scripts/use_starter.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -xEeuo pipefail
 
-OLD_PROJECT_NAME="golang-starter"
+OLD_PROJECT_NAME="dinnerclub"
 NEW_PROJECT_NAME="${1}"
 GITHUB_USERNAME="${2:-toozej}"
 
 GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${GIT_REPO_ROOT}"
 
+# truncate existing CREDITS.md file and replace its contents with link to template repo's CREDITS.md file
+echo -e "# Credits and Acknowledgements\n\n- https://raw.githubusercontent.com/toozej/dinnerclub/main/CREDITS.md" > CREDITS.md
+
 # update go module name
 # shellcheck disable=SC2086
 go mod edit -module=github.com/${GITHUB_USERNAME}/${NEW_PROJECT_NAME}
 
 # move directories
-mv "cmd/${OLD_PROJECT_NAME}" "cmd/${NEW_PROJECT_NAME}"
+#mv "cmd/${OLD_PROJECT_NAME}" "cmd/${NEW_PROJECT_NAME}"
 
 # rename from $OLD_PROJECT_NAME to $NEW_PROJECT_NAME
 grep -rl --exclude-dir=.git/ ${OLD_PROJECT_NAME} . | xargs sed -i "" -e "s/${OLD_PROJECT_NAME}/${NEW_PROJECT_NAME}/g"
 
-# truncate existing CREDITS.md file and replace its contents with link to template repo's CREDITS.md file
-echo "https://raw.githubusercontent.com/toozej/golang-starter/main/CREDITS.md" > CREDITS.md
 
 # show diff output so user can verify their changes
 git diff
